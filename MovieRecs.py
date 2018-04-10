9463a8f3fd0731700bc9a18386b2ee5db5a1ae82
kev <ktran0125gmail.com>
2018-04-10 12:55:42 +0200

    KNN w/ functions

--- MovieRecs.py
+++ MovieRecs.py
@@ -1,9 +1,49 @@
 import pandas as pd
 import numpy as np
+from scipy import spatial
+import operator
 
 r_cols = ["user_id", "movie_id", "rating"] #column titles
 
 ratings = pd.read_csv("/home/kevin/Documents/Data Science/DataScience-Python3/ml-100k/u.data", sep = '\t', names = r_cols, usecols=range(3), encoding = "ISO-8859-1")#link to movieLens dataset
+
+
+movieProperties = ratings.groupby('movie_id').agg({'rating': [np.size, np.mean]})
+
+movieNumRatings = pd.DataFrame(movieProperties['rating']['size'])
+movieNormalizedNumRatings = movieNumRatings.apply(lambda x: (x - np.min(x))/(np.max(x) - np.min(x)))
+
+
+movieDict = {}
+
+with open (r'/home/kevin/Documents/Data Science/DataScience-Python3/ml-100k/u.data') as file_:
+
+    temp = ''
+
+    for line in file_:  #line.decode("ISO-8859-1")
+        fields = line.rstrip('\n').split('|')
+        movieID = int(fields[0])
+        name = fields[1]
+        genres = fields[5:25]
+        genres = map(int, genres)
+        movieDict[movieID] = (name, np.array(list(genres)), movieNormalizedNumRatings.loc[movieID].get('size'),
+        movieProperties.loc[movieID].rating.get('mean'))
+
+
+
+
+
+#compute distance between two movies based on how similar their genres and popularity are
+def ComputeDistance(a, b):
+
+    genresA = a[1]
+    genresB = b[1]
+    genreDistance = spatial.distance.cosine(genresA, genresB)
+    popularityA = a[2]
+    popularityB = b[2]
+    popularityDistance = abs(popularityA - popularityB)
+
+    return genreDistance + popularDistance
 
 m_cols = ["movie_id", "title"]
 
