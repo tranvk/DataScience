8c4342f6034b8f5a22a3e7d0d00c24baeba27db6
kev <ktran0125gmail.com>
2018-04-12 14:16:06 +0200

    First commit

---
+++ KMeans.py
@@ -0,0 +1,26 @@
+from numpy import random, array
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
+from numpy import random, float
+
+#generate fake income and age clusters for N people in k clusters
+def createClusteredData(N, k):
+    random.seed(10)
+    pointsPerCluster = float(N)/k
+
+    X = []
+
+    for i in range (k):
+        incomeCentroid = random.uniform(20000, 200000)
+        ageCentroid = random.uniform(20, 70)
+        for j in range (int(pointsPerCluster)):
+            X.append([random.normal(incomeCentroid, 10000), random.normal(ageCentroid, 2)])
+    X = array(X)
+
+    return X
+
+
+data = createClusteredData(100, 9)
+
+model
