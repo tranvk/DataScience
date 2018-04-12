5a3ebe1a87901f1e03db49983588b99f2e3a5023
kev <ktran0125gmail.com>
2018-04-12 13:41:13 +0200

    adding train/test

--- SpamClassifier.py
+++ SpamClassifier.py
@@ -1,19 +1,22 @@
 import os
 import io
-import numpy
-from pandas import DataFramefrom sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
 
 """
 *Spam classifier using naive bayes (Simplified)
-*
+*TODO: train/test
 """
 
 #read files and parse
 def readFiles(path):
     for root, dirnames, filenames in os.walk(path):
         for name in filenames:
-            path = os.path.join(root, filename)
+            path = os.path.join(root, name)
 
             inBody = False
             lines = []
@@ -25,11 +28,12 @@ def readFiles(path):
                 elif line == '\n':
                     inBody = True
 
-            f.close()
+            file_.close()
             message = '\n'.join(lines)
             yield path, message
 
 
+#populate dataframe from from directory
 def dataFrameFromDirectory(path, classification):
 
     rows = []
@@ -46,13 +50,16 @@ data = DataFrame({'message': [],  'class': []})
 data = data.append(dataFrameFromDirectory('/home/kevin/Documents/Data Science/DataScience-Python3/emails/spam', 'spam'))
 data = data.append(dataFrameFromDirectory('/home/kevin/Documents/Data Science/DataScience-Python3/emails/ham', 'ham'))
 
+#implement train/test
+
+train,test = train_test_split(data, test_size = 0.2)
 
 #split up each message into its list of words
 vectorizer = CountVectorizer()
-counts = vectorizer.fit_transform(data['message'].values) #email contents
+counts = vectorizer.fit_transform(train['message'].values) #email contents
 
 classifier = MultinomialNB()
-targets = data['class'].values
+targets = train['class'].values
 classifier.fit(counts,targets)
 
 #try your code
@@ -60,4 +67,4 @@ classifier.fit(counts,targets)
 examples = [' Viagra free now!!!', " this is spam free", 'sdfasdf', "Hi free viagra Bob, how  free about a game of golf tomorrow?"]
 example_counts = vectorizer.transform(examples)
 predictions = classifier.predict(example_counts)
-predictions
+print(train)
