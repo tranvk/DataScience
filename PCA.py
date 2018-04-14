fe614b7192920a2772dcbaabb53c3e74143ad3f2
kev <ktran0125gmail.com>
2018-04-14 22:51:22 +0200

    Progress

---
+++ PCA.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+from pylab import *
+import pylab as pl
+
+"""
+*Why use PCA: Reduce multi-dimensional data down to fewer dimensions
+*Example: black & white image with X, Y, Brightness axes -> PCA -> image compression/facial recognition
+*How: distills information that contributes most to the variance of data set
+"""
+
+
+#load iris data set, which as 4 dimensions for 3 different kinds of iris flowers
+iris = load_iris()
+
+numSamples, numFeatures = iris.data.shape
+
+#4D -> 2D by projecting it to 2 orthogonal 4D vectors which are basis of new 2D projection
+X = iris.data
+pca = PCA(n_components = 2, whiten = True).fit(X)
+X_pca = pca.transform(X)
